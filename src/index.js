a2a03cdd8e9155b50c23cf2ee0ad964325972930
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2020-11-04 19:07:51 +0200

    finished hw

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const startRef = document.querySelector('[data-action="start"]');
+const stopRef = document.querySelector('[data-action="stop"]');
+let bodyRef = document.querySelector("body");
+let intervalId = null;
+let themeChanging = false;
+
+startRef.addEventListener("click", onStartChange);
+stopRef.addEventListener("click", onStopChange);
+
+function onStartChange() {
+  if (themeChanging) return;
+
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, 6)];
+  }, 1000);
+
+  themeChanging = true;
+}
+
+function onStopChange() {
+  setInterval(clearInterval(intervalId), (themeChanging = false)), 1000;
+}
