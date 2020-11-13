730767a1ae46b33e38a39bceb6caece8b4f48285
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2020-11-13 15:08:00 +0200

    added disabled

--- src/index.js
+++ src/index.js
@@ -15,22 +15,20 @@ const stopRef = document.querySelector('[data-action="stop"]');
 let bodyRef = document.querySelector("body");
 const timeForInterval = 1000;
 let intervalId = null;
-let themeChanging = false;
 
 startRef.addEventListener("click", onStartChange);
 stopRef.addEventListener("click", onStopChange);
 
 function onStartChange() {
-  if (themeChanging) return;
-
   intervalId = setInterval(() => {
     bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, 6)];
   }, timeForInterval);
 
-  themeChanging = true;
+  startRef.disabled = true;
 }
 
 function onStopChange() {
   setInterval(clearInterval(intervalId), (themeChanging = false)),
     timeForInterval;
+  startRef.disabled = false;
 }
